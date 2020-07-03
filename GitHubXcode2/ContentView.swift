ba168a6d5108cc67ca6f0f61cc733be87650254f
Bagus Triyanto <44406611+bagusisgood@users.noreply.github.com>
2020-07-03 23:23:01 +0700

    Initial Commit

---
+++ GitHubXcode2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitHubXcode2
+//
+//  Created by Bagus Triyanto on 03/07/20.
+//  Copyright © 2020 Bagus Triyanto. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
